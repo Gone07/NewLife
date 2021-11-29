6a37b500c51dce5637a3438dc51c3fdf8cb9ef70
Gone07 <aurimas.matu@gmail.com>
2021-11-29 17:26:14 +0200

    fixed passport

--- app/Http/Controllers/StoreController.php
+++ app/Http/Controllers/StoreController.php
@@ -10,7 +10,7 @@ class StoreController extends Controller
 {
     public function __construct()
     {
-        $this->middleware(['auth:api', 'admin'])->only(['destroy']);
+        $this->middleware(['auth:api', 'admin'])->only(['destroy', 'store', 'update']);
     }
 
 
